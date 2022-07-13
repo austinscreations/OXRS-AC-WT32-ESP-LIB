666cd0408ddab105d3e4a62219ea9dff5977ecfc
Ben Jones <ben.jones12@gmail.com>
2022-07-13 13:29:32 +1200

    ready for review/testing

--- src/OXRS_WT32.h
+++ src/OXRS_WT32.h
@@ -5,49 +5,36 @@
 #ifndef OXRS_WT32_H
 #define OXRS_WT32_H
 
-// Macro for converting env vars to strings
-#define STRINGIFY(s) STRINGIFY1(s)
-#define STRINGIFY1(s) #s
-
-#include <ArduinoJson.h>  // json handler
-#include <OXRS_MQTT.h>    // For MQTT
+#include <OXRS_MQTT.h>    // For MQTT pub/sub
 #include <OXRS_API.h>     // For REST API
-#include <WiFi.h>         // Required for Ethernet to get MAC
-#include <MqttLogger.h>   // For logging
 
-#if defined(USESENSORS)
+#if defined(I2C_SENSORS)
 #include <OXRS_SENSORS.h> // For QWICC I2C sensors
 #endif
 
-#if defined(ETHMODE)
-#include <Ethernet.h>     // For networking
-#define DHCP_TIMEOUT_MS          15000
-#define DHCP_RESPONSE_TIMEOUT_MS 4000
-#endif
-
-#if defined(WIFIMODE)
-#include <WiFiManager.h>  // For WiFi AP config
-#endif
+// Ethernet
+#define DHCP_TIMEOUT_MS           15000
+#define DHCP_RESPONSE_TIMEOUT_MS  4000
 
 // REST API
-#define REST_API_PORT            80
+#define REST_API_PORT             80
 
-#ifndef SERIAL_BAUD_RATE
-#define SERIAL_BAUD_RATE         9600
-#endif
-
+// Enum for the different connection states
 enum connectionState_t { CONNECTED_NONE, CONNECTED_IP, CONNECTED_MQTT };
 
 class OXRS_WT32 : public Print
-// class OXRS_WT32
 {
 public:
-  OXRS_WT32();
+  // These are only needed if performing manual configuration in your sketch, otherwise
+  // config is provisioned via the API and bootstrap page
+  void setMqttBroker(const char *broker, uint16_t port);
+  void setMqttClientId(const char *clientId);
+  void setMqttAuth(const char *username, const char *password);
+  void setMqttTopicPrefix(const char *prefix);
+  void setMqttTopicSuffix(const char *suffix);
 
-  void initialiseSerial();
   void begin(jsonCallback config, jsonCallback command);
-  void loop();
-  void restartApi();
+  void loop(void);
 
   // Firmware can define the config/commands it supports - for device discovery and adoption
   void setConfigSchema(JsonVariant json);
@@ -57,17 +44,21 @@ public:
   boolean publishStatus(JsonVariant json);
   boolean publishTelemetry(JsonVariant json);
 
+  // Helpers used to display the connection state on the settings page
+  connectionState_t getConnectionState(void);
   void getIPAddressTxt(char *buffer);
   void getMACAddressTxt(char *buffer);
   void getMQTTTopicTxt(char *buffer);
-
-  connectionState_t getConnectionState(void);
 
   // Implement Print.h wrapper
   virtual size_t write(uint8_t);
   using Print::write;
 
 private:
+  void _initialiseNetwork(byte *mac);
+  void _initialiseMqtt(byte *mac);
+  void _initialiseRestApi(void);
+
   boolean _isNetworkConnected(void);
 };
 
